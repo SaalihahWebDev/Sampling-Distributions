a28af9cf17162c7d59fd9626ba0dee35fcacf954
SaalihahWebDev <bashirabanu91@gmail.com>
2025-09-30 05:22:53 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+puppy_sample=[1,0,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1]
+n=len(puppy_sample)
+blue_eye_proportion=sum(puppy_sample)/n
+print("Sample size:",n)
+print("Number of blue eyed puppies",sum(puppy_sample))
+print("Proportion of Blue eyed puppies",blue_eye_proportion)
